73d9bfedefcef5c998bf60211d57c1ae9f657be5
Julia Bullard <5534@holbertonstudents.com>
2023-03-28 16:57:59 -0500

    Fixed definition names

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,34 +1,39 @@
 #!/usr/bin/python3
 """Task 6"""
 
-from flask import Flask, render_template
+from flask import Flask, escape, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
+def index():
+    """Displays Hello HBNB"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """Displays HBNB"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    return 'C {}'.format(text.replace('_', ' '))
+def c(text):
+    """Displays C"""
+    return 'C {}'.format(escape(text.replace('_', ' ')))
 
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_text(text):
-    return 'Python {}'.format(text.replace('_', ' '))
+def python(text):
+    """Displays Python"""
+    return 'Python {}'.format(escape(text.replace('_', ' ')))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
+    """Displays number"""
     return '{} is a number'.format(n)
 
 
