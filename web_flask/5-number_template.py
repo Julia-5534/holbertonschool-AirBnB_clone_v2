9825b5b95ffc94778280968df89e7192c9f08709
Julia Bullard <5534@holbertonstudents.com>
2023-03-28 16:53:34 -0500

    Updated 5 py file

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -8,33 +8,38 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
+    """Displays Hello HBNB"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """Displays HBNB"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
+    """Displays C"""
     return 'C {}'.format(escape(text.replace('_', ' ')))
 
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text):
+    """Displays Python"""
     return 'Python {}'.format(escape(text.replace('_', ' ')))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
+    """Displays number"""
     return '{} is a number'.format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    return render_template('number_template.html', n=n)
+    return render_template('5-number_template.html', n=n)
 
 
 if __name__ == '__main__':
